7ae261164e9798e4f7d311c17e0a7bcbd94edfcb
CaioSampaio <sampaio.caiooliveira@gmail.com>
2022-11-12 20:23:50 +0000

    Alterações no projeto

--- Proj/Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c
+++ Proj/Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c
@@ -10,7 +10,7 @@
     O programa é compatível o Sistema Operacional Linux; Foi utilizado a distribuição Ubuntu para o desenvolvimento do programa.
 
   Como compilar:
-    Para compilar o programa "Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c" devemos utilizar o compilador C GCC e o seguinte comando no terminal: gcc -g -Wall Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c -o Proj01-04N11-CaioS_GuilhermeP_LeonardoM.bin -Ipthread
+    Para compilar o programa "Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c" devemos utilizar o compilador C GCC e o seguinte comando no terminal: gcc -g -Wall Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c -o Proj02-04N11-CaioS_GuilhermeP_LeonardoM.bin -Ipthread
 
   Como executar o programa:
     Para executar o programa "Proj02-04N11-CaioS_GuilhermeP_LeonardoM.c" devemos utilizar o seguinte comando o terminal: ./Proj02-04N11-CaioS_GuilhermeP_LeonardoM.bin
@@ -44,14 +44,13 @@ struct c{
 #define numero_threads 100
 typedef struct c conta;
 conta from, to;
-int valor;
+int flag = 0;
 
 /* ----- FUNÇÕES AUXILIARES ----- */
 
 // Gera um número aleatório de 1 à 20
 int gerarRandomico (){
-
-    return rand()%20;
+    return rand()%101;
 }
 
 /* ----- FUNÇÕES PRINCIPAIS ----- */
@@ -59,26 +58,52 @@ int gerarRandomico (){
 // Realiza a transferência de valor de uma conta (from) para outra (to)
 void *transferencia(void *arg){
   int i = (intptr_t)arg; //ID de cada transferência
-  
-  printf("\n-- Transferência do nº %d -- \n\n", i + 1);
 
-  valor = gerarRandomico();
-  printf( "Transferindo %d para a conta c2\n", valor );
-	
-	if (from.saldo >= valor){ //2
+  int valor = gerarRandomico();
+
+  while (((valor > from.saldo) && (flag == 0)) || (valor==0)){
+    valor = gerarRandomico();
+  }
+
+	if ((from.saldo >= valor) && (flag == 0)){
+
+    printf("\n-- Transferência do nº %d -- \n\nTransferindo %d\n", i + 1, valor);
+
     from.saldo -= valor;
 		to.saldo += valor;
+
+    if (from.saldo < 0){
+      to.saldo -= valor;
+      from.saldo += valor;
+    }
+
+    if (from.saldo == 0){
+      flag =1;
+    }
 	}
-  valor = gerarRandomico();
-  printf( "Transferindo %d para a conta c1\n", valor );
-  if (to.saldo >= valor){ //2
+
+  while (((valor > to.saldo) && (flag == 1)) || (valor == 0)){
+    valor = gerarRandomico();
+  }
+
+  if ((to.saldo >= valor) && (flag == 1)){
+
+    printf("\n-- Transferência do nº %d -- \n\nTransferindo %d\n", i + 1, valor);
+
 		to.saldo -= valor;
 		from.saldo += valor;
+
+    if (to.saldo < 0){
+      from.saldo -= valor;
+		  to.saldo += valor;
+    }
+
+    if(to.saldo == 0){
+      flag =0;
+    }
 	}
-	
-	printf("Transferência concluída com sucesso!\n");
-	printf("Saldo de c1: %d\n", from.saldo);
-	printf("Saldo de c2: %d\n", to.saldo);
+
+  printf("Transferência %d concluída com sucesso!\nSaldo de c1: $%d\nSaldo de c2: $%d\n", i + 1, from.saldo, to.saldo);
 
   return  0;
 }
@@ -90,29 +115,29 @@ int main(){
 	void* stack;
 	int status;
 	pthread_t transferencias[numero_threads]; //Vetor de transferências como Threads
-	
+
 	//Aloca o stack
 	stack = malloc(FIBER_STACK);
-	
+
 	if (stack == 0){
 		perror("Erro no malloc: não foi possível o stack");
 		exit(1);
 	}
-	
+
 	//Todas as contas começam com saldo 100
 	from.saldo = 100;
-	to.saldo = 100;
+	to.saldo = 0;
 
   //Cria as transferências como threads
 	for (int i = 0; i < numero_threads; i++){
-    
+
     //Timer Fixo de 1 segundos
     //sleep(1);
 
     //Cria cada conta executando o método transferencia e salva o status de criação
     status = pthread_create(&transferencias[i], NULL, transferencia, (void*)(intptr_t)i);
 
-    //Verifica se a criação foi bem-sucedida 
+    //Verifica se a criação foi bem-sucedida
     if(status != 0){
       printf("\nErro na transferência %d\n", i + 1);
     } else{
@@ -120,22 +145,22 @@ int main(){
     }
 	}
 
-    //Espera cada thread (transferência) terminar
-    for(int i = 0; i < numero_threads; i++){
-	
-	    //Realiza o join de cada transferência e salva o status da operação
-      status = pthread_join(transferencias[i], NULL);
-        
-      //Verifica se a operação foi bem-sucedida
-      if(status != 0){
-        printf("A transferência %d apresentou um erro inesperado\n", status);
-      }
+  //Espera cada thread (transferência) terminar
+  for(int i = 0; i < numero_threads; i++){
+
+	  //Realiza o join de cada transferência e salva o status da operação
+    status = pthread_join(transferencias[i], NULL);
+
+    //Verifica se a operação foi bem-sucedida
+    if(status != 0){
+      printf("A transferência %d apresentou um erro inesperado\n", status);
     }
+  }
 
 	//Libera stack
 	free(stack);
-	
+
 	printf("\nTransferências concluídas e memória liberada.\n");
-	
+
 	return 0;
 }
